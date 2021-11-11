2f74c2d7028913c5b7d7e233a3661c494122d51a
Bernardo Fraga <92233318+beFraga@users.noreply.github.com>
2021-11-11 13:37:31 -0300

    Add files via upload

---
+++ add-dir.php
@@ -0,0 +1,39 @@
+<?php
+    function mkName($s){
+        $a = array('à', 'á', 'â', 'ã', 'ä', 'å', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ñ', 'ò', 'ó', 'ô', 'õ', 'ö', 'ù', 'ü', 'ú', 'ÿ', 'À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ñ', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', 'O', 'Ù', 'Ü', 'Ú', ' ');
+        $b = array('a', 'a', 'a', 'a', 'a', 'a', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i', 'n', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'y', 'A', 'A', 'A', 'A', 'A', 'A', 'C', 'E', 'E', 'E', 'E', 'I', 'I', 'I', 'I', 'N', 'O', 'O', 'O', 'O', 'O', 'O', 'U', 'U', 'U', '-');
+        $s = str_replace($a, $b, $s);
+        return strtolower($s);
+    }
+    if(isset($_POST['sub'])){
+        $n = $_POST['dir'];
+        $n = mkName($n);
+        
+        mkdir($n);
+        touch($n.'/index.php');
+        $code = "<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Document</title>
+    <style>
+
+    </style>
+</head>
+<body>
+
+<script>
+
+</script>
+</body>
+</html>";
+    file_put_contents($n.'/index.php', $code);
+    header("Location:./$n");
+    }
+?>
+<form method='post'>
+    <input name='dir' placeholder='Dir name'>
+    <input name='sub' type='submit'>
+</form>
